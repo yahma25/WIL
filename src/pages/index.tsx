a2b346ebe694ffa7297c16d6e2ccd92062aed575
mhkim <yahma25@gmail.com>
2021-09-04 23:02:45 +0900

    Text 컴포넌트는 필요없어져서 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import Text from 'components/Text';
 import { Link } from 'gatsby';
 
 const IndexPage: FunctionComponent = function () {
   return (
     <div>
-      <Text text="Home" />
       <Link to="/info">To Main</Link>
     </div>
   );
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { graphql, Link } from 'gatsby';
-import Text from 'components/Text';
 
 interface InfoPageProps {
   data: {
@@ -23,9 +22,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Text text={title} />
-      <Text text={description} />
-      <Text text={author} />
+      {title} {description} {author}
       <Link to="/">To Main</Link>
     </div>
   );
