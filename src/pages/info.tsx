57a1daa5d95b1c316d04e518a9981e5c8ac9161a
mhkim <yahma25@gmail.com>
2021-09-05 00:16:58 +0900

    React import 생략 가능하도록 설정

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 import { graphql, Link } from 'gatsby';
 import { Global, css } from '@emotion/react';
 
