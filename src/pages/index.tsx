265871638341763726becc36495bcdb4f1450038
mhkim <yahma25@gmail.com>
2021-09-05 01:22:50 +0900

    메인을 자기소개 페이지로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import React, { FunctionComponent } from 'react';
-import { Link } from 'gatsby';
+import { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import GlobalStyle from 'components/common/GlobalStyle';
+import Introduction from 'components/main/Introduction';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Link to="/info">To Main</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+    </Container>
   );
 };
 
