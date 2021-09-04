026a228b597933624ff590742a37d9beaf7be7a8
mhkim <yahma25@gmail.com>
2021-09-05 01:17:03 +0900

    프로필을 보여주는 컴포넌트 추가

---
+++ src/components/main/ProfileImage.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+const PROFILE_IMAGE_PATH = '../../image/profile.jpg';
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`;
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_PATH} alt="Profile Image" />;
+};
+
+export default ProfileImage;
