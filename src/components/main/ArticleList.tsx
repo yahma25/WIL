27f8a83b18cfea1c475420498687353db1502676
mhkim <yahma25@gmail.com>
2021-09-05 14:34:18 +0900

    md 파일의 정보를 기반으로 Article 리스트가 표현되게 수정

--- src/components/main/ArticleList.tsx
+++ src/components/main/ArticleList.tsx
@@ -2,15 +2,22 @@ import { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import ArticleItem from 'components/main/ArticleItem';
 
-const ARTICLE_ITEM_DATA = {
-  title: 'Article Item Title',
-  date: '2021.09.05',
-  categories: ['Work', 'Web'],
-  summary: '경청 잘하는 사람 되는 방법',
-  thumbnail:
-    'https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp',
-  link: '<https://www.google.co.kr/>',
+export type ArticleType = {
+  node: {
+    id: string;
+    frontmatter: {
+      title: string;
+      summary: string;
+      date: string;
+      categories: string[];
+      thumbnail: string;
+    };
+  };
 };
+
+interface ArticleProps {
+  articles: ArticleType[];
+}
 
 const ArticleListWrapper = styled.div`
   display: grid;
@@ -27,13 +34,23 @@ const ArticleListWrapper = styled.div`
   }
 `;
 
-const ArticleList: FunctionComponent = function () {
+const ArticleList: FunctionComponent<ArticleProps> = function ({ articles }) {
   return (
     <ArticleListWrapper>
-      <ArticleItem {...ARTICLE_ITEM_DATA} />
-      <ArticleItem {...ARTICLE_ITEM_DATA} />
-      <ArticleItem {...ARTICLE_ITEM_DATA} />
-      <ArticleItem {...ARTICLE_ITEM_DATA} />
+      {articles.map(
+        ({
+          node: {
+            id,
+            frontmatter: { ...rest },
+          },
+        }) => (
+          <ArticleItem
+            key={id}
+            {...rest}
+            link={'<https://www.google.co.kr/>'}
+          />
+        ),
+      )}
     </ArticleListWrapper>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import GlobalStyle from 'components/common/GlobalStyle';
 import Introduction from 'components/main/Introduction';
 import Footer from 'components/common/Footer';
 import CategoryList from 'components/main/CategoryList';
-import ArticleList from 'components/main/ArticleList';
+import ArticleList, { ArticleType } from 'components/main/ArticleList';
+import { graphql } from 'gatsby';
 
 const Container = styled.div`
   display: flex;
@@ -18,16 +19,49 @@ const CATEGORY_LIST = {
   Web: 3,
 };
 
-const IndexPage: FunctionComponent = function () {
+interface IndexPageProps {
+  data: {
+    allMarkdownRemark: {
+      edges: ArticleType[];
+    };
+  };
+}
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Work" categoryList={CATEGORY_LIST} />
-      <ArticleList />
+      <ArticleList articles={edges} />
       <Footer />
     </Container>
   );
 };
 
 export default IndexPage;
+
+export const queryArticleList = graphql`
+  query queryArticleList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail
+          }
+        }
+      }
+    }
+  }
+`;
