8d0e70a0d8ebd20ea6da15182c8633382544ff85
mhkim <yahma25@gmail.com>
2021-09-17 19:51:19 +0900

    [추가] sitemap 설정하기 위해 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    'gatsby-plugin-sitemap',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
